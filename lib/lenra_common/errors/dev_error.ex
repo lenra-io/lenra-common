768e3d871f05ad056f4f1a6e0e852e65207532a2
Louis G <louis@lenra.me>
2022-07-18 09:41:21 +0200

    add moduledoc

--- lib/lenra_common/errors/dev_error.ex
+++ lib/lenra_common/errors/dev_error.ex
@@ -1,3 +1,9 @@
 defmodule LenraCommon.Errors.DevError do
+  @moduledoc """
+    LenraCommon.Errors.DevErrors Define the DevError structure.
+    This is the error to raise when we are facing an impossible case (a case that should never happen).
+    This error should be catched into sentry.
+  """
+
   use LenraCommon.Errors.ErrorStruct
 end
