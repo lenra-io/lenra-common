502ff10ea2d8e17a0af9cd4ab5a9191e45e89d07
Pichonnier Emric <emric.pichonnier@gmail.com>
2023-02-08 12:35:41 +0100

    feat: log controller

--- lib/lenra_common_web/controllers/fallback_controller.ex
+++ lib/lenra_common_web/controllers/fallback_controller.ex
@@ -1,13 +1,19 @@
 defmodule LenraCommonWeb.FallbackController do
   use LenraCommonWeb, :controller
 
+  require Logger
+
   def call(conn, {:error, reason}) do
+    Logger.debug("#{conn.method} #{conn.request_path} respond error: #{reason}")
+
     conn
     |> assign_error(reason)
     |> reply
   end
 
   def call(conn, {:error, _error, reason, _reason}) do
+    Logger.debug("#{conn.method} #{conn.request_path} respond error: #{reason}")
+
     conn
     |> assign_error(reason)
     |> reply
