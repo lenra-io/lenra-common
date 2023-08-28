37f934b84e6e942c397a5311f05a7314825d265f
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-08-28 17:17:56 +0200

    feat: Create an error logger with stacktrace

---
+++ lib/lenra_common/errors.ex
@@ -0,0 +1,38 @@
+defmodule LenraCommon.Errors do
+  @moduledoc """
+    LenraCommon.Errors manage common errors.
+  """
+  require Logger
+
+  def log(error) when is_struct(error) do
+    [
+      error.message,
+      "\n",
+      format_stacktrace(Process.info(self(), :current_stacktrace)),
+      "\n"
+    ]
+    |> Enum.join()
+    |> Logger.error()
+  end
+
+  def log(error) do
+    [
+      to_string(error),
+      "\n",
+      format_stacktrace(Process.info(self(), :current_stacktrace)),
+      "\n"
+    ]
+    |> Enum.join()
+    |> Logger.error()
+  end
+
+  defp format_stacktrace({:current_stacktrace, stacktrace}) do
+    stacktrace
+    |> Enum.map(&format_stacktrace_line/1)
+    |> Enum.join("\n")
+  end
+
+  defp format_stacktrace_line({module, method, argNum, [file: file, line: line]}) do
+    "\t#{file}:#{line} #{module}.#{method}/#{argNum}"
+  end
+end
